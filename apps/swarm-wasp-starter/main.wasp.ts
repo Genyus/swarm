8f41d94dd82c73c3942900903273e23e0dffd86d
Genyus <genyus@gmail.com>
2026-01-01 14:55:59 -0400

    git subrepo pull (merge) apps/swarm-wasp-starter
    subrepo:
      subdir:   "apps/swarm-wasp-starter"
      merged:   "68ff317"
    upstream:
      origin:   "git@github.com:Genyus/swarm-wasp-starter.git"
      branch:   "main"
      commit:   "6ee6b3a"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "ea10886"

--- apps/swarm-wasp-starter/main.wasp.ts
+++ apps/swarm-wasp-starter/main.wasp.ts
@@ -2,13 +2,14 @@ import { App } from "@ingenyus/swarm-wasp";
 
 const app = await App.create("swarm-wasp-starter", {
   title: "Swarm Wasp Starter",
-  wasp: { version: "^0.18.2" },
+  wasp: { version: "^0.19.0" },
   head: [
     '<meta name="description" content="Swarm Wasp Starter description">',
     '<meta name="viewport" content="width=device-width, initial-scale=1.0">',
     '<meta charset="UTF-8">',
   ],
 });
+
 app.client({
   rootComponent: {
     importDefault: "Layout",
